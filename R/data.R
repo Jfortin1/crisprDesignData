157b867d61391df5c84405b5722f42a88cd5265e
fortinj2 <fortin.jeanphilippe@gene.com>
2022-07-16 09:46:20 -0700

    Done with revamping

--- R/data.R
+++ R/data.R
@@ -1,14 +1,54 @@
-#' Repeat elements coordinates for hg38
+#' Repeat elements coordinates found in the human genome (hg38/GRCh38)
 #'
-#' Repeat elements coordinates for hg38
+#' Repeat elements coordinates for hg38 obtained from the RepeatMasker track
+#' found at the UCSC genome browser. 
 #' 
 #' @format GRanges
 "gr.repeats.hg38"
 
 
-#' Repeat elements coordinates for mm10
+#' Repeat elements coordinates found in the mouse genome (mm10/GRCm38)
 #'
-#' Repeat elements coordinates for mm10
+#' Repeat elements coordinates for mm10 obtained from the RepeatMasker track
+#' found at the UCSC genome browser. 
 #' 
 #' @format GRanges
 "gr.repeats.mm10"
+
+
+#' GRangesList representing Ensembl gene model coordinates for human
+#'
+#' GRangesList representing Ensembl gene model coordinates for human in 
+#' hg38/GRCh38 coordinates (Ensembl release 104)
+#' 
+#' @format GRangesList
+"txdb_human"
+
+
+#' GRangesList representing Ensembl gene model coordinates for mouse
+#'
+#' GRangesList representing Ensembl gene model coordinates for mouse in 
+#' mm10/GRCm38 coordinates (Ensembl release 102)
+#' 
+#' @format GRangesList
+"txdb_mouse"
+
+
+#' GRangesList representing Ensembl gene model coordinates for human
+#'
+#' GRanges representing Ensembl-based TSS coordinates for human in 
+#' hg38/GRCh38 coordinates (Ensembl release 104)
+#' 
+#' @format GRanges
+"tss_human"
+
+
+#' GRanges representing Ensembl-based TSS coordinates for mouse
+#'
+#' GRanges representing Ensembl-based TSS coordinates for mouse in mm10/GRCm38
+#' coordinates (Ensembl release 102)
+#' 
+#' @format GRanges
+"tss_mouse"
+
+
